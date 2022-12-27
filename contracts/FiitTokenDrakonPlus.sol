e4f9dfe54c23f09dad608fedc9f2fb0e51ac2526
juniorkidza <64487792+juniorkidza@users.noreply.github.com>
2022-12-27 15:43:09 +0700

    fix: fix security

--- contracts/FiitTokenDrakonPlus.sol
+++ contracts/FiitTokenDrakonPlus.sol
@@ -11,10 +11,10 @@ import "erc721a/contracts/ERC721A.sol";
 contract FiitTokenDrakonPlus is ERC721A, Ownable {
     using SafeMath for uint256;
     using Counters for Counters.Counter;
+
+    event Withdraw(address owner, uint256 amount);
     
-    Counters.Counter private _tokenIds;
-    
-    uint public constant MAX_SUPPLY = 10000;
+    uint public constant MAX_SUPPLY = 2000;
     uint public constant PRICE = 0.01 ether;
     uint public constant MAX_PER_MINT = 5;
     
@@ -22,10 +22,8 @@ contract FiitTokenDrakonPlus is ERC721A, Ownable {
     
     constructor() ERC721A("FiitTokenDrakon+", "DKP") {}
 
-    function reserveNFTs(uint256 amount) public onlyOwner {
-        uint totalMinted = _tokenIds.current();
-
-        require(totalMinted.add(amount) < MAX_SUPPLY, "Not enough NFTs left to reserve");
+    function reserveNFTs(uint256 amount) external onlyOwner {
+        require(totalSupply() + amount <= MAX_SUPPLY, "Not enough NFTs left to reserve");
 
         _safeMint(msg.sender, amount);
     }
@@ -34,26 +32,26 @@ contract FiitTokenDrakonPlus is ERC721A, Ownable {
         return baseTokenURI;
     }
 
-   function setBaseURI(string memory _baseTokenURI) public onlyOwner {
+   function setBaseURI(string memory _baseTokenURI) external onlyOwner {
         baseTokenURI = _baseTokenURI;
     }
     
-    function mintNFTs(uint _count) public payable {
-        uint totalMinted = _tokenIds.current();
-
-        require(totalMinted.add(_count) <= MAX_SUPPLY, "Not enough NFTs left!");
+    function mintNFTs(uint _count) external onlyOwner payable  {
+        require(totalSupply() + _count <= MAX_SUPPLY, "Not enough NFTs left!");
         require(_count >0 && _count <= MAX_PER_MINT, "Cannot mint specified number of NFTs.");
         require(msg.value >= PRICE.mul(_count), "Not enough ether to purchase NFTs.");
 
         _safeMint(msg.sender, _count);
     }
     
-    function withdraw() public payable onlyOwner {
+    function withdraw() external payable onlyOwner {
         uint balance = address(this).balance;
         require(balance > 0, "No ether left to withdraw");
 
         (bool success, ) = (msg.sender).call{value: balance}("");
         require(success, "Transfer failed.");
+
+        emit Withdraw(msg.sender, balance);
     }
     
 }
